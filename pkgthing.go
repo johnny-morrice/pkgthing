9efe083fd49ae0ce4e52e52911187e086b139c8f
John Morrice <john@functorama.com>
2017-07-20 01:04:15 +0100

    Skeleton search method

--- pkgthing.go
+++ pkgthing.go
@@ -1,22 +1,114 @@
 package pkgthing
 
+import (
+	"github.com/pkg/errors"
+
+	"github.com/johnny-morrice/godless/api"
+	"github.com/johnny-morrice/godless/query"
+)
+
 type Package struct {
+	PackageInfo
+	Blob []byte
 }
 
 type PackageInfo struct {
+	Name       string
+	System     string
+	MetaData   []MetaDataEntry
+	Signatures []Signature
 }
+
+type MetaDataEntry struct {
+	MetaDataKey    string
+	MetaDataValues []string
+}
+
+type SearchKey uint8
+
+const (
+	NameExact = SearchKey(iota)
+)
 
 type PackageSearchTerm struct {
+	SearchKey  SearchKey
+	SearchTerm string
+	Keys       []KeyReference
 }
 
-func Get(deets PackageInfo) (Package, error) {
-	return Package{}, nil
+type PkgThing struct {
 }
 
-func Add(pkg Package) (PackageInfo, error) {
-	return PackageInfo{}, nil
+func (thing *PkgThing) Get(info PackageInfo) (Package, error) {
+	panic("not implemented")
 }
 
-func Search(term PackageSearchTerm) ([]PackageInfo, error) {
-	return nil, nil
+func (thing *PkgThing) Add(pkg Package) (PackageInfo, error) {
+	panic("not implemented")
+}
+
+func (thing *PkgThing) Search(term PackageSearchTerm) ([]PackageInfo, error) {
+	const failMsg = "Search failed"
+
+	builder := &searchBuilder{}
+	err := builder.setSearchTerm(term)
+
+	if err != nil {
+		return nil, errors.Wrap(err, failMsg)
+	}
+
+	query := builder.buildQuery()
+
+	resp, err := thing.sendQuery(query)
+
+	if err != nil {
+		return nil, errors.Wrap(err, failMsg)
+	}
+
+	info, err := readPackageInfo(resp)
+
+	if err != nil {
+		return nil, errors.Wrap(err, failMsg)
+	}
+
+	return info, nil
+}
+
+func (thing *PkgThing) sendQuery(query *query.Query) (api.Response, error) {
+	panic("not implemented")
+}
+
+type KeyType uint16
+
+const (
+	GODLESS_KEY = KeyType(iota)
+)
+
+type SignatureBlob []byte
+
+type KeyFingerprint []byte
+
+type KeyReference struct {
+	Type        KeyType
+	Fingerprint KeyFingerprint
+}
+
+type Signature struct {
+	Fingerprint KeyReference
+	Data        SignatureBlob
+}
+
+type searchBuilder struct {
+}
+
+func (builder *searchBuilder) setSearchTerm(term PackageSearchTerm) error {
+	panic("not implemented")
+}
+
+func (builder *searchBuilder) buildQuery() *query.Query {
+	panic("not implemented")
+}
+
+func readPackageInfo(resp api.Response) ([]PackageInfo, error) {
+	panic("not implemented")
 }
